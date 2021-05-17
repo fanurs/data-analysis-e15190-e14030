8a896e6397bb236239cf3c37aa97d99d55d779ef
Fanurs <teh@nscl.msu.edu>
2021-05-17 02:00:04 -0400

    Added tables.py to submodule e15190/runlog

---
+++ e15190/utilities/tables.py
@@ -0,0 +1,22 @@
+from tabulate import tabulate
+
+def to_fwf(df, path, drop_index=True):
+    """Write Pandas Dataframe into fixed-width file (FWF).
+
+    Parameters
+    ----------
+    df : pandas.Dataframe object
+        The dataframe to be written to file.
+    path : str or pathlib.Path object
+        The path to be written into fixed-width file.
+    drop_index : bool, default True
+        If `True`, the index will not be included;
+        if `False`, the index will be written to file as one of the columns.
+    """
+    df = df.copy()
+    if not drop_index:
+        df.reset_index(inplace=True)
+
+    content = tabulate(df.values.tolist(), list(df.columns), tablefmt='plain')
+    with open(path, 'w') as f:
+        f.write(content)
