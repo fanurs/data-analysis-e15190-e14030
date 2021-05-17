601cd2742b9731115addb23dfc74fdae247245de
Fanurs <teh@nscl.msu.edu>
2021-05-16 22:07:30 -0400

    Rename paths for more consistency

--- e15190/runlog/__init__.py
+++ e15190/runlog/__init__.py
@@ -2,5 +2,5 @@ import pathlib
 
 from .. import PROJECT_DIR
 
-MYSQL_DOWNLOAD_PATH = pathlib.Path(PROJECT_DIR, 'database', 'runlog', 'wmu_mysql_database.h5')
-ELOG_DOWNLOAD_PATH = pathlib.Path(PROJECT_DIR, 'database', 'runlog', 'wmu_elog.html')
+MYSQL_DOWNLOAD_PATH = pathlib.Path(PROJECT_DIR, 'database', 'runlog', 'mysql_database.h5')
+ELOG_DOWNLOAD_PATH = pathlib.Path(PROJECT_DIR, 'database', 'runlog', 'elog.html')
