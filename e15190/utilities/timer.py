53534442ed6e96d1f9c550edeb4121bdd0c01e65
Fanurs <teh@nscl.msu.edu>
2021-05-21 01:46:16 -0400

    Added pause() to VerboseTimer

--- e15190/utilities/timer.py
+++ e15190/utilities/timer.py
@@ -2,9 +2,10 @@ import time
 
 class VerboseTimer:
     def __init__(self, auto_print=True):
-        self.time_start = None
-        self.time_stop = None
-        self.time_diff = None
+        self.status = 'stop' # 'start', 'pause'
+        self.time_start = 0.0
+        self.time_stop = 0.0
+        self.time_diff = 0.0
 
         self.auto_print = auto_print
         self.print_prefix = ''
@@ -12,32 +13,75 @@ class VerboseTimer:
         self.print_kwargs = {'flush': True}
 
     def start(self):
-        self.time_start = time.perf_counter()
+        time_now = time.perf_counter()
 
-    def stop(self):
-        self.time_stop = time.perf_counter()
+        if self.status == 'start':
+            raise Exception('Timer had been started, it cannot be started again.')
+        elif self.status == 'pause' or self.status == 'stop':
+            self.time_start = time_now
 
-        if self.time_start is None:
-            raise Exception('Timer cannot be stopped. Timer was not started.')
+        self.status = 'start'
 
-        self.time_diff = self.time_stop - self.time_start
-        self.reset()
+    def pause(self, show=False):
+        time_now = time.perf_counter()
 
-        if self.auto_print:
+        if self.status == 'start':
+            self.time_diff += time_now - self.time_start
+        elif self.status == 'pause':
+            raise Exception('Timer cannot be paused. Timer had been paused.')
+        elif self.status == 'stop':
+            raise Exception('Timer cannot be paused. Timer was not started.')
+
+        if show:
             self.print()
+        self.status = 'pause'
+
+    def stop(self, show=None, prefix=None, suffix=None):
+        time_now = time.perf_counter()
+
+        if self.status == 'start':
+            self.time_diff += time_now - self.time_start
+        if self.status == 'pause':
+            self.time_diff += 0.0
+        if self.status == 'stop':
+            raise Exception('Timer cannot be stopped. Timer had been stopped.')
+
+        if show is True:
+            self.print()
+        elif show is False:
+            pass
+        elif self.auto_print:
+            self.print(prefix=prefix, suffix=suffix)
+
+        self.reset()
+        self.status = 'stop'
 
     def reset(self):
-        self.time_start = None
-        self.time_stop = None
+        self.status = 'stop'
+        self.time_start = 0.0
+        self.time_stop = 0.0
+        self.time_diff = 0.0
 
-    def print(self):
-        print(self.print_prefix + str(self.time_diff) + self.print_suffix, **self.print_kwargs)
+    def print(self, prefix=None, suffix=None):
+        if prefix is None:
+            prefix = self.print_prefix
+        if suffix is None:
+            suffix = self.print_suffix
+        print(prefix + str(self.time_diff) + suffix, **self.print_kwargs)
 
 verbose_timer = VerboseTimer()
-def start():
+def start(*args, **kwargs):
     global verbose_timer
     verbose_timer.start()
 
-def stop():
+def pause(*args, **kwargs):
     global verbose_timer
-    verbose_timer.stop()
+    verbose_timer.pause()
+
+def stop(*args, **kwargs):
+    global verbose_timer
+    verbose_timer.stop(*args, **kwargs)
+
+def reset(*args, **kwargs):
+    global verbose_timer
+    verbose_timer.reset(*args, **kwargs)
