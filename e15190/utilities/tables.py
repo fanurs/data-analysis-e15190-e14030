35b5a28feee257a8f03abad9992d92cc3ef7011e
Fanurs <teh@nscl.msu.edu>
2021-08-03 16:17:20 -0400

    :hammer: Added drop_header

--- e15190/utilities/tables.py
+++ e15190/utilities/tables.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 
-def to_fwf(df, path, drop_index=True, **tabulate_kwargs):
+def to_fwf(df, path, drop_header=False, drop_index=True, **tabulate_kwargs):
     """Write Pandas Dataframe into fixed-width file (FWF).
 
     Parameters
@@ -9,6 +9,9 @@ def to_fwf(df, path, drop_index=True, **tabulate_kwargs):
         The dataframe to be written to file.
     path : str or pathlib.Path object
         The path to be written into fixed-width file.
+    drop_header : bool, default False
+        If `True`, table header will be dropped; if `False`, `df.columns` will
+        be used as header.
     drop_index : bool, default True
         If `True`, the index will not be included; if `False`, the index will be
         written to file as one of the columns.
@@ -20,8 +23,9 @@ def to_fwf(df, path, drop_index=True, **tabulate_kwargs):
     if not drop_index:
         df.reset_index(inplace=True)
 
+    header = [] if drop_header else list(df.columns)
     kw = dict(tablefmt='plain')
     kw.update(tabulate_kwargs)
-    content = tabulate(df.values.tolist(), list(df.columns), **kw)
+    content = tabulate(df.values.tolist(), header, **kw)
     with open(path, 'w') as f:
         f.write(content + '\n')
