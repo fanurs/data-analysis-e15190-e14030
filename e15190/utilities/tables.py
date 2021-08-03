e01b3de3e505d43c3743e02c88340a463d6a1a7d
Fanurs <teh@nscl.msu.edu>
2021-08-03 16:11:55 -0400

    :sparkles: Added tabulate_kwargs

--- e15190/utilities/tables.py
+++ e15190/utilities/tables.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 
-def to_fwf(df, path, drop_index=True):
+def to_fwf(df, path, drop_index=True, **tabulate_kwargs):
     """Write Pandas Dataframe into fixed-width file (FWF).
 
     Parameters
@@ -10,13 +10,18 @@ def to_fwf(df, path, drop_index=True):
     path : str or pathlib.Path object
         The path to be written into fixed-width file.
     drop_index : bool, default True
-        If `True`, the index will not be included;
-        if `False`, the index will be written to file as one of the columns.
+        If `True`, the index will not be included; if `False`, the index will be
+        written to file as one of the columns.
+    tabulate_kwargs
+        Keyword arguments for the tabulate function. See more at
+        https://pypi.org/project/tabulate/
     """
     df = df.copy()
     if not drop_index:
         df.reset_index(inplace=True)
 
-    content = tabulate(df.values.tolist(), list(df.columns), tablefmt='plain')
+    kw = dict(tablefmt='plain')
+    kw.update(tabulate_kwargs)
+    content = tabulate(df.values.tolist(), list(df.columns), **kw)
     with open(path, 'w') as f:
         f.write(content + '\n')
