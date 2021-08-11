aa60aa08eb29dfe010ea4dab066c4c04a47e5852
Fanurs <teh@nscl.msu.edu>
2021-08-10 23:19:38 -0400

    :see_no_evil: Ignore local/etc

--- local/etc/conda/activate.d/activate-env_vars.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ ! -z "$PROJECT_DIR" ]; then
-    export OLD_PROJECT_DIR=$PROJECT_DIR
-fi
-export PROJECT_DIR="/mnt/spirit/analysis/user/fanurs/e15190-e14030/data-analysis-e15190-e14030"
--- local/etc/conda/deactivate.d/deactivate-env_vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -z "$OLD_PROJECT_DIR" ]; then
-    PROJECT_DIR=$OLD_PROJECT_DIR
-    unset OLD_PROJECT_DIR
-else
-    unset PROJECT_DIR
-fi
