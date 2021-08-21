5d0375e29c1a6f7e256ccf938fbec4690df9ea18
Fanurs <teh@nscl.msu.edu>
2021-08-20 23:38:11 -0400

    :zap: Improved performance using numpy.einsum()

--- e15190/utilities/ray_triangle_intersection.py
+++ e15190/utilities/ray_triangle_intersection.py
@@ -1,20 +1,18 @@
+import functools
+import itertools
+
 import numpy as np
 import plotly.graph_objects as go
+import sympy as sp
 
-def moller_trumbore(ray_origin, ray_vectors, triangles, tol=1e-9):
+def moller_trumbore(ray_origin, ray_vectors, triangles, mode='einsum', tol=1e-9):
     """Implementation of Moller-Trumbore ray-triangle intersection algorithm
     To read more about the mathematical derivation, visit
     https://www.scratchapixel.comhttps://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection.
-    In this implementation, we have made some light optimization. When looping
-    over the triangles, we use pure Python for-loop. Inside the loop, we use
-    only NumPy functions to manage all the ray_vectors, which are much faster
-    than looping over with pure Python loops. This optimization, of course,
-    works the best when dealing with a large number of ray vectors (> 1,000,000)
-    rather than numerous triangles.
 
     Parameters:
         ray_origin : array of shape (3, )
-            This implementation assumes all rays have one common origin.
+            This implementation only allows all rays to have one common origin.
         ray_vectors : array of shape (n_rays, 3)   
             The ray vectors are only used to tell the directions. No
             normalization is needed.
@@ -23,7 +21,14 @@ def moller_trumbore(ray_origin, ray_vectors, triangles, tol=1e-9):
             vertices that specify the triangle. For example, the y-coordinate of
             the first vertex in the fifth triangle would be
             `triangles[4][0][1]`.
-        tol : float, default 1e-8
+        mode : str, default `'einsum'`
+            To select different implementations of the algorithm. In 'einsum'
+            mode, the `numpy.einsum()` function is heavily used to optimize the
+            calculations. Otherwise, we will use the implementation without
+            'einsum'. Our simple test suggests, for 1 million rays and 12
+            triangles, 'einsum' mode performs at least two times faster than the
+            mode without 'einsum'.
+        tol : float, default 1e-9
             Tolerance for checking the determinant in the algorithm. If the
             determinant is smaller than the tolerance, then the ray would be
             assumed parallel to the plane of the triangle, hence no attempt
@@ -32,9 +37,64 @@ def moller_trumbore(ray_origin, ray_vectors, triangles, tol=1e-9):
     Returns:
         A numpy.ndarray of intersections with shape (n_triangles, n_rays, 3).
     """
+    args = (ray_origin, ray_vectors, triangles, tol)
+    if mode == 'einsum':
+        return _moller_trumbore_with_einsum(*args)
+    else:
+        return _moller_trumbore_with_loop_over_triangles(*args)
+
+def _moller_trumbore_with_einsum(ray_origin, ray_vectors, triangles, tol):
     ray_origin, ray_vectors, triangles = map(lambda x: np.array(x), [ray_origin, ray_vectors, triangles])
 
-    dot = lambda x, y: np.sum(np.multiply(x, y), axis=1) # allowing broadcasting
+    eijk = [sp.LeviCivita(*ijk) for ijk in itertools.product(range(1, 4), repeat=3)]
+    eijk = np.reshape(eijk, (3, 3, 3)).astype(float)
+    einsum = functools.partial(np.einsum, optimize='optimal')
+
+    edge_01 = triangles[:, 1] - triangles[:, 0] # (tri, 3)
+    edge_02 = triangles[:, 2] - triangles[:, 0] # (tri, 3)
+    translation = ray_origin - triangles[:, 0] # (tri, 3)
+
+    # einsum allows us to skip intermediate vectors, vec_p and vec_q
+    # which improves the performance significantly.
+    # For future reference, here are how vec_p and vec_q can be calculated:
+    #    >> vec_p = einsum('ijk,rj,tk->tri', eijk, ray_vectors, edge_02)
+    #    >> vec_q = einsum('ijk,tj,tk->ti', eijk, translation, edge_01)
+
+    # denominator = vec_p cross edge_01
+    denominator = einsum( # shape = (tri, ray, 3)
+        'ijk,rj,tk,ti->tr',
+        eijk, ray_vectors, edge_02, edge_01 # ==> vec_p, edge_01
+    )
+    non_parallel = (np.abs(denominator) > tol) # (tri, ray, 3)
+    denominator[~non_parallel] = tol # to avoid divide by zero warning
+    scalar = 1 / denominator
+
+    # u = vec_p dot translation / denominator
+    u = einsum( # shape = (tri, ray)
+        'ijk,rj,tk,ti,tr->tr',
+        eijk, ray_vectors, edge_02, translation, scalar, # ==> vec_p, translation, scalar
+    )
+
+    # v = vec_q dot ray_vectors / denominator
+    v = einsum( # shape = (tri, ray)
+        'ijk,tj,tk,ri,tr->tr',
+        eijk, translation, edge_01, ray_vectors, scalar, # ==> vec_q, ray_vectors, scalar
+    )
+
+    # t = vec_q dot edge_02 / denominator
+    t = einsum( # shape = (tri, ray)
+        'ijk,tj,tk,ti,tr->tr',
+        eijk, translation, edge_01, edge_02, scalar, # ==> vec_q, edge_02, scalar
+    )
+    intersected = (0 < u) & (u < 1) & (v > 0) & (u + v < 1)
+    t[(~intersected) | (t <= tol)] = 0.0
+
+    return ray_origin + einsum('tr,ri->tri', t, ray_vectors) # shape = (tri, ray, 3)
+
+def _moller_trumbore_with_loop_over_triangles(ray_origin, ray_vectors, triangles, tol):
+    ray_origin, ray_vectors, triangles = map(lambda x: np.array(x), [ray_origin, ray_vectors, triangles])
+
+    dot = lambda x, y: np.sum(np.multiply(x, y), axis=1) # to allow broadcasting
 
     intersections = []
     for triangle in triangles:
@@ -49,8 +109,8 @@ def moller_trumbore(ray_origin, ray_vectors, triangles, tol=1e-9):
         non_parallel = (np.abs(denominator) > tol)
         denominator[~non_parallel] = tol # to avoid divide by zero warning
 
-        u = np.where(non_parallel, dot(vec_p, translation) / denominator, -1e9)
-        v = np.where(non_parallel, dot(vec_q, ray_vectors) / denominator, -1e9)
+        u = dot(vec_p, translation) / denominator
+        v = dot(vec_q, ray_vectors) / denominator
 
         intersected = (0 < u) & (u < 1) & (v > 0) & (u + v < 1)
         t = np.where(intersected, np.dot(vec_q, edge_02) / denominator, -1e9)[:, None]
