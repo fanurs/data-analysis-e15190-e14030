15dfeacaa9a40596dd1b976bc1c0d6fdcea82510
Fanurs <teh@nscl.msu.edu>
2021-09-14 01:07:22 -0400

    :memo: Explicitly write down the arguments

--- e15190/utilities/fast_histogram.py
+++ e15190/utilities/fast_histogram.py
@@ -5,12 +5,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 @functools.wraps(fh.histogram1d)
-def histo1d(*args, **kwargs):
-    return fh.histogram1d(*args, **kwargs)
+def histo1d(x, range, bins, weights=None):
+    """A wrapper function for fast_histogram.histogram1d()
+
+    Parameters:
+        x : array-like
+            Input data.
+        range : 2-tuple or 2-list
+            The lower and upper range of the bins.
+        bins : int
+            The number of bins.
+        weights : array-like or None, default None
+            Weights for each value in `a`.
+
+    Returns:
+        An array of bin values.
+    """
+    return fh.histogram1d(x, range=range, bins=bins, weights=weights)
 
 @functools.wraps(fh.histogram2d)
-def histo2d(*args, **kwargs):
-    return fh.histogram2d(*args, **kwargs)
+def histo2d(x, y, range, bins, weights=None):
+    """A wrapper function for fast_histogram.histogram2d()
+
+    Parameters:
+        x : array-like, shape (N,)
+            Input data x.
+        y : array-like, shape (N,)
+            Input data y.
+        range : array-like, shape (2, 2)
+            The lower and upper range of the bins: `[[xmin, xmax], [ymin, ymax]]`.
+        bins : [int, int]
+            The number of bins: `[x_bins, y_bins]`.
+        weights : array-like of shape (N,) or None, default None
+            Weights for each (x, y) pair.
+        
+    Returns:
+        A two-dimensional array containing the bin values in shape `(x_bins, y_bins)`.
+    """
+    return fh.histogram2d(x, y, range=range, bins=bins, weights=weights)
 
 @functools.wraps(plt.hist)
 def plot_histo1d(hist_func, x, range, bins, **kwargs):
