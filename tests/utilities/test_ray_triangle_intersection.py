96327b8aa30f32baa0c829a0f32006e30733344b
Fanurs <teh@nscl.msu.edu>
2021-09-21 17:51:31 -0400

    :white_check_mark: Passed test for emit_isotropic_rays()

--- tests/utilities/test_ray_triangle_intersection.py
+++ tests/utilities/test_ray_triangle_intersection.py
@@ -1,7 +1,9 @@
 import pytest
 
 import numpy as np
+from scipy import stats
 
+from e15190.utilities import geometry as geom
 from e15190.utilities import ray_triangle_intersection as rti
 
 @pytest.fixture()
@@ -36,3 +38,50 @@ def test_consistency_between_modes(synthetic_data):
         )
         print(result_einsum)
         assert np.allclose(result_einsum, result_raw)
+
+def test_emit_isotropic_rays():
+    # check shape
+    n_rays = np.random.randint(low=5, high=15)
+    rays = rti.emit_isotropic_rays(n_rays)
+    assert rays.shape == (n_rays, 3)
+
+    # check random_seed
+    cart_rays_s_ref = rti.emit_isotropic_rays(10, random_seed=0)
+    cart_rays_s0 = rti.emit_isotropic_rays(10, random_seed=0)
+    cart_rays_s1 = rti.emit_isotropic_rays(10, random_seed=1)
+    assert np.allclose(cart_rays_s_ref, cart_rays_s0)
+    assert not np.allclose(cart_rays_s_ref, cart_rays_s1)
+
+    # the rest are all checks for isotropy
+    n_rays = int(1e5) # must be sufficient to pass all statistical tests
+    pvalue = 0.01
+    uniform_cdf = lambda low, upp: stats.uniform(loc=low, scale=(upp - low)).cdf
+    kstest = stats.kstest # alias
+
+    # check isotropy in cartesian coordinates
+    cart_rays = rti.emit_isotropic_rays(n_rays, frame='cartesian')
+    for i in range(3):
+        assert kstest(cart_rays[:, i], uniform_cdf(-1, 1)).pvalue > pvalue
+    
+    # check isotropy in spherical coordinates
+    sphr_rays = rti.emit_isotropic_rays(n_rays, frame='spherical')
+    assert np.allclose(sphr_rays[:, 0], 1.0)
+    assert kstest(sphr_rays[:, 1], lambda x: 0.5 * (1 - np.cos(x))).pvalue > pvalue
+    assert kstest(sphr_rays[:, 2], uniform_cdf(-np.pi, np.pi)).pvalue > pvalue
+
+    # check isotropy in some subregion
+    cart_rays = rti.emit_isotropic_rays(n_rays, polar_range=[0, np.pi / 3])
+    sphr_rays = geom.cartesian_to_spherical(cart_rays)
+    assert kstest(cart_rays[:, 0], uniform_cdf(-1, 1)).pvalue < pvalue # not uniform
+    assert kstest(cart_rays[:, 1], uniform_cdf(-1, 1)).pvalue < pvalue # not uniform
+    assert kstest(cart_rays[:, 2], uniform_cdf(0.5, 1)).pvalue > pvalue
+    assert kstest(sphr_rays[:, 1], lambda x: 2 * (1 - np.cos(x))).pvalue > pvalue
+    assert kstest(sphr_rays[:, 2], uniform_cdf(-np.pi, np.pi)).pvalue > pvalue
+
+    cart_rays = rti.emit_isotropic_rays(n_rays, azimuth_range=[-0.5 * np.pi, 0.5 * np.pi])
+    sphr_rays = geom.cartesian_to_spherical(cart_rays)
+    assert kstest(cart_rays[:, 0], uniform_cdf(0, 1)).pvalue > pvalue
+    assert kstest(cart_rays[:, 1], uniform_cdf(-1, 1)).pvalue > pvalue
+    assert kstest(cart_rays[:, 2], uniform_cdf(-1, 1)).pvalue > pvalue
+    assert kstest(sphr_rays[:, 1], lambda x: 0.5 * (1 - np.cos(x))).pvalue > pvalue
+    assert kstest(sphr_rays[:, 2], uniform_cdf(-0.5 * np.pi, 0.5 * np.pi)).pvalue > pvalue
