5b4ea180f3215175d365c948e37f2889ca40cb18
Fanurs <teh@nscl.msu.edu>
2022-04-28 15:39:45 -0400

    :construction: Cosmic re-analysis in progress

--- e15190/neutron_wall/cosmic.py
+++ e15190/neutron_wall/cosmic.py
@@ -25,7 +25,7 @@ class CosmicRun:
     DATABASE_DIR = '$DATABASE_DIR/neutron_wall/cosmic'
     CACHE_RELPATH = r'cache/run-{run:04d}.db'
 
-    def __init__(self, AB: Literal['A', 'B'], min_multi=8):
+    def __init__(self, AB: Literal['A', 'B'], min_multi=5):
         self.AB = AB.upper()
         self.ab = AB.lower()
         self.min_multi = min_multi
@@ -49,12 +49,10 @@ class CosmicRun:
         kw = dict(
             from_cache=from_cache,
             sql_cmd='WHERE ' + ' AND '.join([
-                'VW_multi == 0',
                 f'NW{self.AB}_multi >= {self.min_multi}',
                 f'NW{self.AB}_time_L - NW{self.AB}_time_R < 20',
                 f'NW{self.AB}_time_L - NW{self.AB}_time_R > -20',
             ]),
-            drop_columns=['VW_multi'],
         )
         kw.update(kwargs)
 
@@ -62,7 +60,6 @@ class CosmicRun:
         self.df = rc.read(
             self.runs,
             {
-                'VetoWall.fmulti'                           : 'VW_multi',
                 f'NW{self.AB}.fmulti'                       : f'NW{self.AB}_multi',
                 f'NW{self.AB}.fnumbar'                      : f'NW{self.AB}_bar',
                 f'NW{self.AB}.fLeft'                        : f'NW{self.AB}_total_L',
@@ -142,7 +139,7 @@ class TrackFitter:
         linear = RANSACRegressor(
             min_samples=0.8,
             max_trials=10,
-            residual_threshold=20.0,
+            residual_threshold=30.0,
         ).fit(y[:, None], x)
         pca = PCA(n_components=1).fit(np.vstack([y, x]).T[linear.inlier_mask_])
         return [len(x), *pca.mean_[::-1], *pca.components_[0][::-1]]
@@ -240,7 +237,7 @@ cr.get_tracks()
 cr.calculate_track_angles()
 
 #%%
-i = 14
+i = 25
 df = cr.df.copy()
 indices = df.reset_index('subentry').index.unique()
 entry = df.loc[indices[i]]
@@ -259,11 +256,24 @@ plt.show()
 
 #%%
 class UshapeRansacEstimator:
-    def __init__(self):
-        pass
+    AMBE_PATH = '$DATABASE_DIR/neutron_wall/ambe/compton_edges_3072-3073.csv'
 
-    def model(self, x, a, b, c):
-        return (a * x + b)**4 + c
+    def __init__(self, bar):
+        self.bar = bar
+        self.ambe_fit = self._get_ambe_fit(bar)
+
+    @classmethod
+    def _get_ambe_fit(cls, bar, with_intercept=False):
+        path = Path(os.path.expandvars(cls.AMBE_PATH))
+        df = pd.read_csv(path).set_index('bar')
+        par = df.loc[bar]
+        if with_intercept:
+            return lambda x: par[0] + par[1] * x + par[2] * x**2
+        else:
+            return lambda x: par[1] * x + par[2] * x**2
+    
+    def model(self, x, y_shift):
+        return self.ambe_fit(x) + y_shift
 
     def fit(self, X, y):
         self.par, _ = curve_fit(self.model, X.flatten(), y)
@@ -281,26 +291,96 @@ class UshapeRansacEstimator:
         return 1 - u / v
     
     def get_params(self, deep=True):
-        return dict()
+        return dict(bar=self.bar)
     
     def set_params(self, **params):
-        return UshapeRansacEstimator()
+        return UshapeRansacEstimator(self.bar)
 
 #%%
-bar = 12
-df_bar = cr.df.query(f'NWB_bar == {bar} & NWB_total_L < 4000 & NWB_total_R < 4000 & pos_x < 110 & pos_x > -110')
+path = '$DATABASE_DIR/neutron_wall/light_output_calibration/nwb_pulse_height_calibration.dat'
+path = os.path.expandvars(path)
+df_light = pd.read_csv(path, delim_whitespace=True, comment='#').set_index('bar')
+def get_light_from_adc(adc, pos, a, b, c, d, e):
+    result = 4.196 * adc / (a + b * pos + c * pos**2)
+    result = d + result * e
+    return result
+
+#%%
+class UshapeFitter:
+    def __init__(self, bar, cosmic_run):
+        self.bar = bar
+        self.AB = cosmic_run.AB
+        self.df = cosmic_run.df.query(' & '.join([
+            f'NW{self.AB}_bar == {self.bar}',
+            f'NW{self.AB}_total_L < 4000',
+            f'NW{self.AB}_total_R < 4000',
+            f'NW{self.AB}_total_L > 100',
+            f'NW{self.AB}_total_R > 100',
+        ]))
+        self.df = self.df.reset_index('subentry')
+        self.df_tracks = cosmic_run.df_tracks
+    
+    def get_angle_cut(self, angle, half_width=10.0):
+        track_idx = self.df_tracks.query(f'abs(angle - {angle}) < {half_width}').index
+        idx = self.df.index.intersection(track_idx)
+        return self.df.loc[idx].set_index('subentry', append=True)
+    
+    def fit(self, angle, half_width=10.0):
+        subdf = self.get_angle_cut(angle, half_width)
+        subdf = subdf.query('abs(pos_x) < 60')
+        fitter = RANSACRegressor(
+            base_estimator=UshapeRansacEstimator(self.bar),
+            min_samples=0.8,
+            max_trials=100,
+            residual_threshold=150,
+        )
+        fitter.fit(
+            np.array(subdf.pos_x)[:, None],
+            np.sqrt(subdf[f'NW{self.AB}_total_L'] * subdf[f'NW{self.AB}_total_R'])
+        )
+        return fitter.estimator_.par[0]
+
+for bar in range(1, 25):
+    u_fitter = UshapeFitter(bar, cr)
+    x = [4.2, 11.96, 17.0536, 22.1550]
+    y = [UshapeRansacEstimator._get_ambe_fit(bar, True)(0)]
+    angles = [0, 44.4, 56.3]
+    for angle in angles:
+        y.append(u_fitter.fit(angle))
+
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, s=3)
+    x_plt = np.linspace(0, 4000, 100)
+    light = get_light_from_adc(x_plt, 0, *df_light.loc[bar])
+    plt.plot(light, x_plt, color='red')
+    ax.set_title(bar)
+    ax.set_xlim(-1, 80)
+    ax.set_ylim(-100, 4100)
+    ax.grid()
+    plt.show()
+
+
+#%%
+bar = 7
+df_bar = cr.df.query(f'NWB_bar == {bar} & NWB_total_L < 4000 & NWB_total_R < 4000 & pos_x < 120 & pos_x > -120')
 df_bar = df_bar.reset_index('subentry')
-indices = df_bar.index.intersection(cr.df_tracks.query('abs(angle - 0) < 10').index)
-# indices = df_bar.index.intersection(cr.df_tracks.query('abs(angle - 56.3) < 10').index)
+# indices = df_bar.index.intersection(cr.df_tracks.query('abs(angle - 0) < 10').index)
+# indices = df_bar.index.intersection(cr.df_tracks.query('abs(angle - 44.4) < 10').index)
+indices = df_bar.index.intersection(cr.df_tracks.query('abs(angle - 56.3) < 10').index)
 df_bar = df_bar.loc[indices].set_index('subentry', append=True)
 df_bar
 
-u_fitter = RANSACRegressor(base_estimator=UshapeRansacEstimator(), min_samples=0.5, max_trials=100)
-u_fitter.fit(
-    np.array(df_bar.pos_x)[:, None],
-    np.sqrt(df_bar.NWB_total_L * df_bar.NWB_total_R),
+u_fitter = RANSACRegressor(
+    base_estimator=UshapeRansacEstimator(bar),
+    min_samples=0.8,
+    max_trials=100,
+    residual_threshold=150,
 )
-print(u_fitter.predict(np.array([[0]])))
+df_fit = df_bar.query('pos_x < 60 & pos_x > -60')
+u_fitter.fit(
+    np.array(df_fit.pos_x)[:, None],
+    np.sqrt(df_fit.NWB_total_L * df_fit.NWB_total_R),
+)
 print(u_fitter.estimator_.par)
 
 fig, ax = plt.subplots()
@@ -315,7 +395,7 @@ fh.plot_histo2d(
     df_bar.pos_x,
     np.sqrt(df_bar.NWB_total_L * df_bar.NWB_total_R),
     range=[[-120, 120], [0, 4000]],
-    bins=[50, 50],
+    bins=[100, 100],
     cmap='viridis',
     norm=mpl.colors.LogNorm(vmin=1),
 )
@@ -325,3 +405,13 @@ ax.set_ylabel(r'$\sqrt{A_L\cdot A_R} / 4.196$')
 plt.show()
 
 #%%
+subdf = df_bar.query('pos_x > -10 & pos_x < 10')
+fig, ax = plt.subplots()
+fh.plot_histo1d(
+    ax.hist,
+    np.sqrt(subdf.NWB_total_L * subdf.NWB_total_R),
+    range=[0, 2000],
+    bins=50,
+    histtype='step',
+)
+plt.show()
