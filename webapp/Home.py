c1dd7d4eac6bbc64a417ad1b33e156efc9fbb115
Fanurs <teh@nscl.msu.edu>
2022-12-07 16:49:55 -0500

    :sparkles: Streamlit app launch

---
+++ webapp/Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Web app for E15190-E14030')
+
+st.write('An interactive approach to data visualization.')
---
+++ webapp/pages/1_Overview.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+"""# Overview"""
+
+"""(under construction)"""
+
